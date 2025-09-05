e214c1d553080b9d3dc068f08af96c6ce5f0a723
“Tristian <tristian.davis@atlasschool.com>
2025-09-05 13:11:20 -0500

    updated links with my github/linkedin, and added alts

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,10 +1,10 @@
 function SocialLinks() {
 	return (
 		<div className="social-links">
-			<a href="" target="_blank">
+			<a href="https://www.linkedin.com/in/tristiandavis/" target="_blank" alt="linked-in">
 				<img src="/src/assets/linkedin.svg" />
 			</a>
-			<a href="" target="_blank">
+			<a href="https://github.com/TebariousBag/atlas-react-and-jsx" target="_blank" alt="github-repo">
 				<img src="/src/assets/github.svg" />
 			</a>
 		</div>
