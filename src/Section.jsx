59ba3a958e9cef75aa39e2cd4d66fb8170a3b19f
“Tristian <tristian.davis@atlasschool.com>
2025-09-05 13:42:36 -0500

    section component

---
+++ src/Section.jsx
@@ -0,0 +1,11 @@
+// Section component
+function Section({ title, children }) {
+  return (
+    <div className="section">
+      <h1>{title}</h1>
+      {children}
+    </div>
+  );
+}
+
+export default Section;
