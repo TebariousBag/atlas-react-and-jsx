82ebfc5570d4e3c510bda6ca126f5f750be22821
“Tristian <tristian.davis@atlasschool.com>
2025-09-05 15:46:57 -0500

    some images werent showing on netlify, I was sourcing to assets folder, fixed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,9 @@
+import profilePic from '../assets/profile-pic.jpeg';
+
 function AboutMe() {
 	return (
 		<div className="about-me">
-			<img src="/src/assets/profile-pic.jpeg"></img>
+			<img src={profilePic} alt="Tristian Davis profile picture" />
 			<p>My name is Tristian Davis. I am a full-stack/web development student in my final trimester of school. I really enjoy working with front end technologies. I think a lot of what JavaScript can do is pretty cool, and so far I am enjoying learning React. I'm hoping to find a job in a field where I can be creative and do some front end creating. In my free time I enjoy drawing and playing with different animation programs.</p>
 		</div>
 	)
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,11 +1,14 @@
+import linkedinIcon from '../assets/linkedin.svg';
+import githubIcon from '../assets/github.svg';
+
 function SocialLinks() {
 	return (
 		<div className="social-links">
 			<a href="https://www.linkedin.com/in/tristiandavis/" target="_blank" alt="linked-in">
-				<img src="/src/assets/linkedin.svg" />
+				<img src={linkedinIcon} alt="LinkedIn" />
 			</a>
 			<a href="https://github.com/TebariousBag/atlas-react-and-jsx" target="_blank" alt="github-repo">
-				<img src="/src/assets/github.svg" />
+				<img src={githubIcon} alt="GitHub" />
 			</a>
 		</div>
 	);
