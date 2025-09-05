6004c5984d0080e4673d8de989b02e40ba2bcd43
“Tristian <tristian.davis@atlasschool.com>
2025-09-05 14:54:46 -0500

    helpful resource updated and working, and updated the content on app also

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Section from './components/Section';
 import OpenLink from './components/OpenLink';
 import CopyLink from './components/CopyLink';
+import HelpfulResource from './components/HelpfulResource';
 
 function App() {
   return (
@@ -26,6 +27,11 @@ function App() {
           <li>JSX: React often uses JSX (JavaScript XML), a syntax extension that allows writing HTML-like code within JavaScript, making it easier to define UI structures within components.</li>
           <li>Unidirectional Data Flow: React typically follows a unidirectional data flow, where data flows in a single direction (from parent components to child components), simplifying debugging and state management.</li>
         </ul>
+      </Section>
+      <Section title="Helpful resources">
+        <HelpfulResource link="https://www.w3schools.com/REACT/DEFAULT.ASP" label="W3 Schools - Learning by Examples" />
+        <HelpfulResource link="https://www.codecademy.com/learn/react-101" label="Code Academy - React 101" />
+        <HelpfulResource link="https://www.geeksforgeeks.org/reactjs/react/" label="Geeks for Geeks - React Tutorial" />
       </Section>
     </div>
   );
