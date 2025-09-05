7fda97031f2e45b41b0d1b761bb98c8282c6e975
“Tristian <tristian.davis@atlasschool.com>
2025-09-05 15:30:06 -0500

    removed my test tags from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,6 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <OpenLink link="https://www.google.com" />
-      <CopyLink link="https://www.googledoodoo.com" />
       <Section title="What is React?">
         <p>
           React is an open-source JavaScript library for building user interfaces (UIs), particularly for single-page applications. It is maintained by Facebook and a community of individual developers and companies. React allows 
