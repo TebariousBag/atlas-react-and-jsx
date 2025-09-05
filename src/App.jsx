567cf5496ee9cbdd0b484d99bb23146ea9f9ab80
“Tristian <tristian.davis@atlasschool.com>
2025-09-05 13:07:36 -0500

    social links work. I need to remember that it needs to be wrapped in a single element

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 // import the greeting we created
 import Greeting from "./components/Greeting";
+// import SocialLinks we created
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
-  return <div className="app"><Greeting /></div>;
+  return (
+    <div>
+  <div className="app"><Greeting /></div>
+  <div className="social-links"><SocialLinks /></div>
+  </div>
+  );
 }
 
 export default App;
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,14 @@
+function SocialLinks() {
+	return (
+		<div className="social-links">
+			<a href="" target="_blank">
+				<img src="/src/assets/linkedin.svg" />
+			</a>
+			<a href="" target="_blank">
+				<img src="/src/assets/github.svg" />
+			</a>
+		</div>
+	);
+}
+
+export default SocialLinks;
